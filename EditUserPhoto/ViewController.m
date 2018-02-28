51b8f5850700b39d145c35a33425b109ec1de400
yu <>
2018-02-28 17:55:55 +0800

    删除多余代码

--- EditUserPhoto/ViewController.m
+++ EditUserPhoto/ViewController.m
@@ -14,14 +14,7 @@
 
 @property (nonatomic,strong) EditUserPhotoView * editUserPhotoView;
 
-@property (nonatomic,strong) UIButton * remakesButton;
-
-@property (nonatomic,strong) UIButton * exportDataButton;
-
-@property (nonatomic,strong) UIButton * uploadDataButton;
-
 @property (nonatomic,strong) EditUserSquareView * selectEditUserSquareView;
-
 
 @end
 
@@ -32,30 +25,6 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     [self.view addSubview:self.editUserPhotoView];
-//    [self.view addSubview:self.remakesButton];
-//    [self.view addSubview:self.exportDataButton];
-//    [self.view addSubview:self.uploadDataButton];
-}
-
-- (void)remakesButAction {
-    self.editUserPhotoView = nil;
-    [self.editUserPhotoView removeFromSuperview];
-    [self.view addSubview:self.editUserPhotoView];
-}
-
-- (void)exportDataButtonAction {
-    
-}
-
-- (void)uploadDataButtonAction {
-    NSMutableArray * arrayImage = [[NSMutableArray alloc] init];
-    [arrayImage addObject:@"http://7vzoqx.com1.z0.glb.clouddn.com/00014da1677b8ea062056c4949ccc911.jpeg"];
-    [arrayImage addObject:@"http://7vzoqx.com1.z0.glb.clouddn.com/00019e87ce44f48fd443e6b1b399b49f.jpeg"];
-    [arrayImage addObject:@"http://7vzoqx.com1.z0.glb.clouddn.com/0001fff6da7febafcd433fcc6c05853c.jpeg"];
-    [arrayImage addObject:@"http://7vzoqx.com1.z0.glb.clouddn.com/0002517de13e064276a37dc17eb4e47b.jpeg"];
-    [arrayImage addObject:@"http://7vzoqx.com1.z0.glb.clouddn.com/0003493831fdd92ee5a6dd05672e9702.jpeg"];
-
-    [self.editUserPhotoView refreshImageData:[arrayImage copy] sortID:[arrayImage copy]];
 }
 
 #pragma mark - EditUserPhotoViewDelegate
@@ -155,41 +124,6 @@
     }
     return _editUserPhotoView;
 }
-
-- (UIButton *)remakesButton {
-    if (_remakesButton == nil) {
-        _remakesButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_remakesButton setTitle:@"重制" forState:UIControlStateNormal];
-        [_remakesButton addTarget:self action:@selector(remakesButAction) forControlEvents:UIControlEventTouchUpInside];
-        _remakesButton.frame = CGRectMake(10, self.editUserPhotoView.frame.size.height + self.editUserPhotoView.frame.origin.y  + 10, self.view.frame.size.width - 20, 44);
-        _remakesButton.backgroundColor = [UIColor grayColor];
-    }
-    return _remakesButton;
-}
-
-
-- (UIButton *)exportDataButton {
-    if (_exportDataButton == nil) {
-        _exportDataButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_exportDataButton setTitle:@"导出" forState:UIControlStateNormal];
-        [_exportDataButton addTarget:self action:@selector(exportDataButtonAction) forControlEvents:UIControlEventTouchUpInside];
-        _exportDataButton.frame = CGRectMake(10, self.remakesButton.frame.size.height + self.remakesButton.frame.origin.y  + 10, self.view.frame.size.width - 20, 44);
-        _exportDataButton.backgroundColor = [UIColor grayColor];
-    }
-    return _exportDataButton;
-}
-
-- (UIButton *)uploadDataButton {
-    if (_uploadDataButton == nil) {
-        _uploadDataButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_uploadDataButton setTitle:@"上传" forState:UIControlStateNormal];
-        [_uploadDataButton addTarget:self action:@selector(uploadDataButtonAction) forControlEvents:UIControlEventTouchUpInside];
-        _uploadDataButton.frame = CGRectMake(10, self.exportDataButton.frame.size.height + self.exportDataButton.frame.origin.y  + 10, self.view.frame.size.width - 20, 44);
-        _uploadDataButton.backgroundColor = [UIColor grayColor];
-    }
-    return _uploadDataButton;
-}
-
 
 
 - (void)didReceiveMemoryWarning {
